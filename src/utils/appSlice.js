2d9324cbc144a9242a39395b891d753fde252763
Tejas Kharat <tejaskharat870@gmail.com>
2025-04-12 15:38:10 +0530

    Small changes

--- src/utils/appSlice.js
+++ src/utils/appSlice.js
@@ -9,12 +9,15 @@ const appSlice = createSlice({
     toggleMenu: (state, action) => {
       state.isMenuOpen = !state.isMenuOpen;
     },
+    openMenu: (state, action) => {
+      state.isMenuOpen = true;
+    },
     closeMenu: (state, action) => {
       state.isMenuOpen = false;
     }
   }
 })
 
-export const {toggleMenu, closeMenu} = appSlice.actions;
+export const {toggleMenu, closeMenu, openMenu} = appSlice.actions;
 
 export default appSlice.reducer;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,1 @@
-const GOOGLE_API_KEY = 'AIzaSyAKBeykyQ3yje7OrN3UjZEJaoUqUV5F4Qs'
-
-export const YOUTUBE_VIDEOS_API = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key='+ GOOGLE_API_KEY;
-
+export const YOUTUBE_VIDEOS_API = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key='+ process.env.REACT_APP_GOOGLE_API_KEY;
